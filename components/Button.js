897d28a75f7e0e8211ce0dc6f5b8c9418ba3f7a0
José Manuel Domínguez <68101685+jmdc89@users.noreply.github.com>
2023-02-20 15:50:08 +0300

    Button
    Hero import

--- components/Button.js
+++ components/Button.js
@@ -1,6 +1,6 @@
 import React from 'react'
 
-function Button() {
+function Button({children,className,onClick}) {
   return (
     <button
     className={`p-2 rounded-md hover:ring-2 hover:ring-gray-300 ${className}`}
--- sections/Hero.js
+++ sections/Hero.js
@@ -1,11 +1,12 @@
+import Button from "@/components/Button"
 
 const Hero = () => {
   return (
     <section className="mt-16">
-    <h1 className="text-7xl font-bold">Hi I' m Jose</h1>
-    <h3 className="text-4xl my-3">I am a web Developer</h3>
-    <p className="text-gray-700 mb-8">Lorem ipsum Lorem ipsum Lorem ipsum Lorem ipsum Lorem ipsum</p>
-    <button>Hire Me!</button>
+      <h1 className="text-7xl font-bold">Hi I' m Jose</h1>
+      <h3 className="text-4xl my-3">I am a web Developer</h3>
+      <p className="text-gray-700 mb-8">Lorem ipsum Lorem ipsum Lorem ipsum Lorem ipsum Lorem ipsum</p>
+      <Button>Hire Me!</Button>
     </section>
 
   )
