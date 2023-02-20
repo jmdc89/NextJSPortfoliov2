d60ea1f0d00d7a782c04ff8915028cba7d336d75
José Manuel Domínguez <68101685+jmdc89@users.noreply.github.com>
2023-02-20 09:27:21 +0300

    Hero section
    init and import

--- sections/Hero.js
+++ sections/Hero.js
@@ -1,7 +1,12 @@
 
 const Hero = () => {
   return (
-    <div>Hero</div>
+    <section>
+    <h1>Hi I' m Jose</h1>
+    <h3>I am a web Developer</h3>
+    <p>Lorem ipsum Lorem ipsum Lorem ipsum Lorem ipsum Lorem ipsum</p>
+    </section>
+
   )
 }
 
