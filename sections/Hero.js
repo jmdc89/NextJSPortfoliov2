b3180a13547f278147386e3e9d6075fd308827d5
José Manuel Domínguez <68101685+jmdc89@users.noreply.github.com>
2023-02-20 15:52:24 +0300

    Update Hero.js
    padding

--- sections/Hero.js
+++ sections/Hero.js
@@ -6,7 +6,9 @@ const Hero = () => {
       <h1 className="text-7xl font-bold">Hi I' m Jose</h1>
       <h3 className="text-4xl my-3">I am a web Developer</h3>
       <p className="text-gray-700 mb-8">Lorem ipsum Lorem ipsum Lorem ipsum Lorem ipsum Lorem ipsum</p>
-      <Button>Hire Me!</Button>
+      <Button
+        className="bg-purple-600 text-white px-6"
+      >Hire Me!</Button>
     </section>
 
   )
