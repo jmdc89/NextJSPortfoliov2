bca186e1078765a5f685d00d2d15eef558a42171
José Manuel Domínguez <68101685+jmdc89@users.noreply.github.com>
2023-02-20 16:14:10 +0300

    Update Header.js
    moon icon

--- sections/Header.js
+++ sections/Header.js
@@ -1,9 +1,21 @@
+import Button from "@/components/Button"
 import { motion } from "framer-motion"
 import { SocialIcon } from "react-social-icons"
 
 
 
 const Header = () => {
+
+  const renderThemeChanger = () => {
+    return (
+        <Button>
+              <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className="w-6 h-6">
+              <path strokeLinecap="round" strokeLinejoin="round" d="M21.752 15.002A9.718 9.718 0 0118 15.75c-5.385 0-9.75-4.365-9.75-9.75 0-1.33.266-2.597.748-3.752A9.753 9.753 0 003 11.25C3 16.635 7.365 21 12.75 21a9.753 9.753 0 009.002-5.998z" />
+              </svg>
+        </Button>
+    )
+  }
+
  
   return (
     <header className="sticky top-0 p-5 flex items-start justify-between max-w-7xl mx-auto z-100 xl:items-center">
@@ -67,17 +79,7 @@ const Header = () => {
 
           className="flex flex-row items-center">
 
-          {/* <DarkModeSwitch
-            style={{}}
-            checked={isDarkMode}
-            onChange = {handleDarkModeChange}
-            // onChange={() => setTheme(theme === 'light' ? 'dark' : 'light')}
-            size={50}
-            moonColor={"gray"}
-            sunColor={'black'}
-          /> */}
-
-          {/* <DarkModeButton /> */}
+          {renderThemeChanger()}
 
       </motion.div>
 
