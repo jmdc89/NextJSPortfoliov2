cf67abda86a1252cc35236b24e021399dee56f7d
José Manuel Domínguez <68101685+jmdc89@users.noreply.github.com>
2023-02-21 15:40:11 +0300

    Update Footer.js
    dark mode

--- sections/Footer.js
+++ sections/Footer.js
@@ -2,7 +2,7 @@
 const Footer = () => {
   return (
     <footer className="py-6 text-center text-gray-500 text-sm">
-      <span className="text-gray-900 font-bold text-lg mr-2">Cand</span>&copy; {new Date().getFullYear()} All Rights Reserved
+      <span className="text-gray-900 dark:text-white font-bold text-lg mr-2">Cand</span>&copy; {new Date().getFullYear()} All Rights Reserved
     </footer>
   )
 }
