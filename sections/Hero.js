42fadf025fe0d8c4a0d28b687579c9cb5bbb6583
José Manuel Domínguez <68101685+jmdc89@users.noreply.github.com>
2023-02-22 16:09:55 +0300

    hero section
    css

--- sections/Hero.js
+++ sections/Hero.js
@@ -3,32 +3,32 @@ import {Cursor, useTypewriter} from "react-simple-typewriter";
 
 const Hero = () => {
 
-  // const [text, count] = useTypewriter({
-  //     words: [
-  //       "Hi, The name's Jose",
-  //       "Coffee Lover",
-  //       "Code lover"
-  //     ],
-  //     loop: true,
-  //     delay: 2000,
-  // });
+  const [text, count] = useTypewriter({
+      words: [
+        "Hi, The name's Jose",
+        "Coffee Lover",
+        "Code lover"
+      ],
+      loop: true,
+      delay: 2000,
+  });
 
   return (
-    <section className="mt-16">
-      <h1 className="text-7xl font-bold">Hi I' m Jose</h1>
-      <h3 className="text-4xl my-3">I am a web Developer</h3>
-      <p className="text-gray-700 mb-8 dark:text-yellow-300">Lorem ipsum Lorem ipsum Lorem ipsum Lorem ipsum Lorem ipsum</p>
-      <Button
-        className="bg-purple-600 dark:bg-yellow-400 text-white px-6"
-      >Hire Me!</Button>
-    </section>
+    // <section className="mt-16">
+    //   <h1 className="text-7xl font-bold">Hi I' m Jose</h1>
+    //   <h3 className="text-4xl my-3">I am a web Developer</h3>
+    //   <p className="text-gray-700 mb-8 dark:text-yellow-300">Lorem ipsum Lorem ipsum Lorem ipsum Lorem ipsum Lorem ipsum</p>
+    //   <Button
+    //     className="bg-purple-600 dark:bg-yellow-400 text-white px-6"
+    //   >Hire Me!</Button>
+    // </section>
 
-    // <div>
-    //   <h1>
-    //     <span>{text}</span>
-    //     <Cursor cursorColor="blue" />
-    //   </h1>
-    // </div>
+    <div>
+      <h1>
+        <span className="text-blue-900 dark:text-red-200">{text}</span>
+        <Cursor cursorColor="blue" />
+      </h1>
+    </div>
 
   )
 }
