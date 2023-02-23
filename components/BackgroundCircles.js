268aa4ccda3026ff507490f564e695517540f36e
José Manuel Domínguez <68101685+jmdc89@users.noreply.github.com>
2023-02-23 08:43:42 +0300

    Update BackgroundCircles.js
    Circles css

--- components/BackgroundCircles.js
+++ components/BackgroundCircles.js
@@ -3,11 +3,11 @@ import React from 'react'
 function BackgroundCircles() {
   return (
     <div className='relative flex justify-center items-center'>
-        <div className='absolute border border-red-500 rounded-full h-[200px] w-[200px] mt-52 animate-ping'/>
-        <div className='absolute border border-red-500 rounded-full h-[300px] w-[300px] mt-52'/>
-        <div className='absolute border border-red-500 rounded-full h-[500px] w-[500px] mt-52'/>
-        <div />
-        <div />
+        <div className='absolute border border-[#333333] rounded-full h-[200px] w-[200px] mt-52 animate-ping'/>
+        <div className='absolute border border-[#333333] rounded-full h-[300px] w-[300px] mt-52'/>
+        <div className='absolute border border-[#333333] rounded-full h-[500px] w-[500px] mt-52'/>
+        <div className='absolute border border-[#F7AB0A] opacity-20 rounded-full h-[650px] w-[650px] mt-52 animate-pulse'/>
+        <div className='absolute border border-[#333333] opacity-20 rounded-full h-[800px] w-[800px] mt-52 animate-pulse'/>
     </div>
   )
 }
