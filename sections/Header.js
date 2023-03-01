bfefb58ebd8ec3b8320669564e2dd3e40406375d
José Manuel Domínguez <68101685+jmdc89@users.noreply.github.com>
2023-03-01 15:39:42 +0300

    hover css
    hover icons

--- sections/Header.js
+++ sections/Header.js
@@ -75,7 +75,7 @@ const Header = () => {
 
         <svg
             xmlns="http://www.w3.org/2000/svg"
-            className="h-5 w-5 fill-black dark:fill-gray-400"
+            className="h-5 w-5 fill-black dark:fill-gray-400 dark:hover:fill-[#F7AB0A] hover:fill-blue-900"
             fill="currentColor"
             viewBox="0 0 24 24">
             <path
@@ -84,7 +84,7 @@ const Header = () => {
 
         <svg
           xmlns="http://www.w3.org/2000/svg"
-          className="h-5 w-5 fill-black dark:fill-gray-400"
+          className="h-5 w-5 fill-black dark:fill-gray-400 dark:hover:fill-[#F7AB0A] hover:fill-blue-900"
           fill="currentColor"
           viewBox="0 0 24 24">
           <path
@@ -92,7 +92,7 @@ const Header = () => {
         </svg>
 
         <svg 
-          className="h-6 w-6 text-black dark:text-gray-400"  
+          className="h-6 w-6 text-black dark:text-gray-400 hover:text-blue-900 dark:hover:text-[#F7AB0A]"
           viewBox="0 0 24 24"  fill="none"  
           stroke="currentColor"  strokeWidth="2"  
           strokeLinecap="round"  
