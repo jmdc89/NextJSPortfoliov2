58365ed03635cd5bfc682d654fc35126c80d2ef1
José Manuel Domínguez <68101685+jmdc89@users.noreply.github.com>
2023-03-05 09:39:52 +0300

    Update About.js
    image css

--- sections/About.js
+++ sections/About.js
@@ -5,7 +5,7 @@ import AboutImage from '../images/about.jpg'
 
 function About() {
   return (
-    <div className='flex flex-col relative h-screen text-center md:text-left  md:flex-row max-w-7xl px-10 justify-evenly mx-auto items-center'>
+    <div className='flex flex-col relative h-screen text-center md:text-left  md:flex-row max-w-7xl px-10 justify-evenly mx-auto items-center pointer-events-none'>
         <h3 className='absolute top-24 uppercase tracking-[20px] dark:text-gray-500 text-2xl'>
             About
         </h3>
@@ -22,11 +22,14 @@ function About() {
           viewport={{once:true}}
         >
         <Image
-          className="mb-20 md:mb-0 flex-shrink-0 w-56 h-56 rounded-full object-cover md:rounded-lg md:w-64 md:h-64 xl:w-[300px] xl:h-[400px]"
+          className="mt-20 mb-20 md:mb-0 flex-shrink-0 w-56 h-56 rounded-full object-cover md:rounded-lg md:w-64 md:h-64 xl:w-[300px] xl:h-[400px]"
           src={AboutImage}
         />
         </motion.div>
         
+        <div className='space-y-10 px-0 md:px-10'>
+          <h4 className='text-4xl font-semibold'>Here is a little background</h4>
+        </div>
 
 
     </div>
