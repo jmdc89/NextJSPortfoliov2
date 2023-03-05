23787dad8d7b17564173c46124f8b0fbda2d4c83
José Manuel Domínguez <68101685+jmdc89@users.noreply.github.com>
2023-03-05 08:49:49 +0300

    About
    init

--- pages/index.js
+++ pages/index.js
@@ -1,11 +1,20 @@
+import About from "@/components/About";
 import Hero from "@/sections/Hero";
 
 export default function Home() {
   return (
-
+    <>
+    
     <section id="hero" className="snap-center">
       <Hero />
     </section>
+
+    <section id="hero" className="snap-center">
+      <About />
+    </section>
+
+    </>
+    
 
   )
 }
