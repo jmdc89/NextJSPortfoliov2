c76de87bcd3081ea75577a6c34f0b51c092657f7
José Manuel Domínguez <68101685+jmdc89@users.noreply.github.com>
2023-03-05 11:04:00 +0300

    snap and scroll

--- pages/index.js
+++ pages/index.js
@@ -5,11 +5,13 @@ export default function Home() {
   return (
     <>
 
-    <section id="hero" className="snap-start">
+    {/* <section id="hero" className="snap-start"> */}
+    <section id="hero">
       <Hero />
     </section>
 
-    <section id="about" className="snap-center">
+    {/* <section id="about" className="snap-center"> */}
+    <section id="about">
       <About />
     </section>
 
