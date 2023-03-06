07cc67830a0007fcd6eb9b31b4412a95f3cc334a
José Manuel Domínguez <68101685+jmdc89@users.noreply.github.com>
2023-03-06 14:24:55 +0300

    css
    experience

--- components/ExperienceCard.js
+++ components/ExperienceCard.js
@@ -8,7 +8,7 @@ import unespImage from '../images/unesp.png'
 
 function ExperienceCard() {
   return (
-    <article className='flex  flex-col rounded-lg items-center space-y-7 flex-shrink-0 w-[500px] md:w-[600px] xl:w-[900px] snap-center bg-[#292929] p-10 hover:opacity-100 opacity-40 cursor-pointer transition-opacity duration-200 overflow-hidden'>
+    <article className='flex  flex-col rounded-lg items-center space-y-7 flex-shrink-0 w-[500px] md:w-[500px] xl:w-[500px] snap-center bg-[#292929] p-10 hover:opacity-100 opacity-40 cursor-pointer transition-opacity duration-200 overflow-hidden'>
         <motion.div
           initial={{
             y: -100,
@@ -19,16 +19,16 @@ function ExperienceCard() {
           }}
           whileInView={{opacity:1, y:0}}
           viewport={{once:true}}
-          className=""
+          className="my-0"
         >
         <Image
-          className="w-32 h-32 rounded-full xl:w-[200px] xl:h-[200px] object-cover object-center"
+          className="w-32 h-32 rounded-full object-cover object-center"
           src={aiuImage}
           alt="aiu logo"
         />
         </motion.div>
 
-        <div className='px-0 md:px-10'>
+        <div className='px-0 md:px-5'>
           <h4 className='text-4xl font-light'>FabLab Coordinator</h4>
           <p className='font-bold text-2xl mt-1'>American International University</p>
           <div className='flex space-x-2 my-2'>
--- sections/WorkExperience.js
+++ sections/WorkExperience.js
@@ -8,8 +8,8 @@ function WorkExperience() {
       initial={{opacity:0}}
       whileInView={{opacity:1}}
       transition={{duration:1.5}}
-      className='h-screen flex relative overflow-hidden flex-col text-left md:flex-row max-w-full px-10 justify-evenly mx-auto items-center'>
-        <h3 className='absolute top-24 uppercase tracking-[20px] dark:text-gray-500 text-2xl'>
+      className='h-screen flex relative overflow-hidden flex-col text-left max-w-full px-10 justify-evenly mx-auto items-center'>
+        <h3 className='mt-20 mb-30 inline-flex uppercase tracking-[20px] dark:text-gray-500 text-2xl'>
             Experience
         </h3>
 
