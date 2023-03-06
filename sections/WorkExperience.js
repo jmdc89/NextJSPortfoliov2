ef1ef35bb3e11f47a78a7b9180f115dd4dbeefeb
José Manuel Domínguez <68101685+jmdc89@users.noreply.github.com>
2023-03-06 09:29:17 +0300

    Update WorkExperience.js
    card horizontal scroll

--- sections/WorkExperience.js
+++ sections/WorkExperience.js
@@ -13,7 +13,7 @@ function WorkExperience() {
             Experience
         </h3>
 
-        <div>
+        <div className='w-full flex space-x-5 overflow-x-scroll p-10 snap-x snap-mandatory'>
           <ExperienceCard />
           <ExperienceCard />
           <ExperienceCard />
