f0bd2736ea6bc5fecf5fc13b1460c5a0c70c0658
José Manuel Domínguez <68101685+jmdc89@users.noreply.github.com>
2023-03-07 08:46:46 +0300

    Skill section init
    init

--- sections/Skills.js
+++ sections/Skills.js
@@ -1,5 +1,6 @@
 import React from 'react'
 import { motion } from 'framer-motion'
+import Skill from '@/components/Skill'
 
 function Skills() {
   return (
@@ -12,6 +13,15 @@ function Skills() {
         <h3 className='absolute top-36 uppercase tracking-[3px] text-gray-500 text-sm'>
             Hover over a skill for current proficiency
         </h3>
+
+        <div className='grid grid-cols-4 gap-5'>
+            <Skill/>
+            <Skill/>
+            <Skill/>
+            <Skill/>
+            <Skill/>
+        </div>
+
     </motion.div>
   )
 }
