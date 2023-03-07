ef881cc343f1b4a6284ffcda72b2ea9127da2ff5
José Manuel Domínguez <68101685+jmdc89@users.noreply.github.com>
2023-03-07 15:43:46 +0300

    Skills
    css transition

--- components/Skill.js
+++ components/Skill.js
@@ -1,12 +1,26 @@
 import React from 'react'
 import { motion } from 'framer-motion'
+import Image from "next/image";
 import ThDLogo from '../images/3dprintinglogo.png'
 import cncLogo from '../images/cnclogo.png'
 
 
-function Skill() {
+function Skill({directionLeft}) {
   return (
-    <div>Skill</div>
+    <motion.div
+    initial={{
+      x: directionLeft ? -200 : 200,
+      opacity:0
+    }}
+    whileInView={{opacity:1, x: 0}}
+    transition={{duration:1}}
+    className='group relative flex cursor-pointer'
+    >
+      <Image
+          className="w-56 h-56 rounded-full object-cover md:rounded-lg md:w-64 md:h-64 xl:w-[300px] xl:h-[350px]"
+          src={ThDLogo}
+        />
+    </motion.div>
   )
 }
 
--- sections/Skills.js
+++ sections/Skills.js
@@ -14,11 +14,11 @@ function Skills() {
             Skills
         </h3>
 
-        <h3 className='absolute top-36 uppercase tracking-[3px] text-gray-500 text-sm'>
+        <h3 className='absolute mt-10 top-36 uppercase tracking-[3px] text-gray-500 text-sm'>
             Hover over a skill for current proficiency
         </h3>
 
-        <div className='grid grid-cols-4 gap-5'>
+        <div className='absolute top-60 grid grid-cols-4 gap-5'>
             <Skill/>
             <Skill/>
             <Skill/>
