a80cec96f3367e0a77be2a37341f352e0bb73eee
José Manuel Domínguez <68101685+jmdc89@users.noreply.github.com>
2023-03-07 09:04:29 +0300

    import
    import

--- components/Skill.js
+++ components/Skill.js
@@ -1,5 +1,8 @@
 import React from 'react'
 import { motion } from 'framer-motion'
+import ThDLogo from '../images/3dprintinglogo.png'
+import cncLogo from '../images/cnclogo.png'
+
 
 function Skill() {
   return (
