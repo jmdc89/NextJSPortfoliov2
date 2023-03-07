e62d67e596d26468616a2958bef42dd869f9d5c2
José Manuel Domínguez <68101685+jmdc89@users.noreply.github.com>
2023-03-07 08:25:12 +0300

    pointer events
    update

--- sections/About.js
+++ sections/About.js
@@ -9,7 +9,7 @@ function About() {
       initial={{opacity:0}}
       whileInView={{opacity:1}}
       transition={{duration:1.5}}
-      className='flex flex-col relative h-screen text-center md:text-left  md:flex-row max-w-7xl px-10 justify-evenly mx-auto mb-20 items-center pointer-events-none'>
+      className='flex flex-col relative h-screen text-center md:text-left  md:flex-row max-w-7xl px-10 justify-evenly mx-auto mb-20 items-center'>
         <h3 className='md:absolute top-24 uppercase tracking-[20px] dark:text-gray-500 text-2xl'>
             About.<span className='dark:text-[#F7AB0A] text-blue-900'>ME</span>
         </h3>
